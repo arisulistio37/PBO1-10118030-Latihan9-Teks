839c4565c721a7eefed2acbbc630f5cb194582af
arisulistio37 <arisulistio37@gmail.com>
2019-09-28 17:24:43 +0700

    Initial commit

---
+++ src/pbo1/pkg10118030/latihan9/teks/PBO110118030Latihan9Teks.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118030.latihan9.teks;
+
+/**
+ *
+ * @author 
+ * NAMA     : Ari Sulistio
+ * KELAS    : IF-1
+ * NIM      : 10118030
+ */
+public class PBO110118030Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" +newline + "Char : " +tab + "berisi "+c);
+    }
+    
+}
